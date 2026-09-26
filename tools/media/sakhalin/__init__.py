376e3d49fdef66f7d2101dfcc2bcd9570ab0c1e3
vsnetkov <valera.snetkov@gmail.com>
2026-09-26 20:03:21 +1100

    feat: add media provider package

---
+++ tools/media/sakhalin/__init__.py
@@ -0,0 +1,17 @@
+"""Sakhalin Kids media-provider boundary."""
+
+from .provider import (
+    GenerationKind,
+    GenerationRequest,
+    GenerationResult,
+    GenerationStatus,
+    MediaProvider,
+)
+
+__all__ = [
+    "GenerationKind",
+    "GenerationRequest",
+    "GenerationResult",
+    "GenerationStatus",
+    "MediaProvider",
+]
